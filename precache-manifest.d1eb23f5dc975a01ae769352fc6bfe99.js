0fc2c61e84b6b9a580c5c89c0347ff48c75ca4c2
Nicolas MYLLE <millt.nico@hotmail.fr>
2020-03-28 00:35:39 +0100

    Updates

---
+++ precache-manifest.d1eb23f5dc975a01ae769352fc6bfe99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1b872c113589493344cabcd84c3ae1e",
+    "url": "/moonside/index.html"
+  },
+  {
+    "revision": "db2d582efb2a6ac7e3c6",
+    "url": "/moonside/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "71d4900bcec53f920f98",
+    "url": "/moonside/static/js/2.2a1edbc6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moonside/static/js/2.2a1edbc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2d582efb2a6ac7e3c6",
+    "url": "/moonside/static/js/main.bac96334.chunk.js"
+  },
+  {
+    "revision": "e2200001ca1155631c0c",
+    "url": "/moonside/static/js/runtime-main.aa097ad8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/moonside/static/media/logo.5d5d9eef.svg"
+  }
+]);
